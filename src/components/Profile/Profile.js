8176352552546d3902df172404342de152ed57db
Klyment <klim86kr@gmail.com>
2023-05-22 22:04:09 +0300

    fixc

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,24 +4,29 @@
   const Profile = ({ username, tag,location, avatar, stats }) => {
     return Profile.map((card) => {
         return(
-            <div key = {card.id}
-            className='card container profile'
-            style= {{width: '18rem'}} >
-            
-            <div class="description">
-                <img
-                       src={card.avatar}
-                className='card-img-avatar'
-                alt={card.title}/>
-
-                {/* <p class="name">Petra Marica</p>
-                <p class="tag">@pmarica</p>
-                <p class="location">Salvador, Brasil</p> */}
+            <div className={css.profile}>
+            <div className={css.description}>
+                <img src={avatar} alt={username} width="300" className={css.avatar} />
+                <p className={css.name}>{username}</p>
+                <p className={css.tag}>@{tag}</p>
+                <p className={css.location}>{location}</p>
             </div>
-            <ul class="stats">
-        
+
+            <ul className={css.stats}>
+                <li>
+                <span className={css.label}>Followers</span>
+                <span className={css.quantity}>{stats.followers}</span>
+                </li>
+                <li>
+                <span className={css.label}>Views</span>
+                <span className={css.quantity}>{stats.views}</span>
+                </li>
+                <li>
+                <span className={css.label}>Likes</span>
+                <span className={css.quantity}>{stats.likes}</span>
+                </li>
             </ul>
-        </div>
+            </div>
         )
     });
   
